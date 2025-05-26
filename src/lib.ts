e3679d21ded83957fc068bb3418b367a2a10e108
sgs-senthilsb <senthil.sb@sgstechsolution.com>
2025-05-26 17:22:21 +0530

    update for eval

--- src/lib.ts
+++ src/lib.ts
@@ -199,7 +199,7 @@ export function locate(ctx: Page | FrameLocator, input: string): Locator {
     loc = eval(code)
   } else if (input.startsWith("!!!")) {
     const parts = input.substring(3).split("|")
-    loc = ctx.getByText(parts[0] as any)
+    loc = ctx.getByText(parts[0] as any).first()
   } else if (input.startsWith("!!")) {
     const parts = input.substring(2).split("|")
     loc = ctx.getByPlaceholder(parts[0] as any)
